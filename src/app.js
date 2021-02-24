da1d241d662a05a7aca362211040f5a6024638fd
moise <mose.ns@hotmail.com>
2021-02-24 23:52:29 +0100

    data5

--- src/app.js
+++ src/app.js
@@ -1,14 +1,8 @@
 const fetctData = async () => {
-  const res = await fetch('http://localhost:3000/training');
-  const data = await res.json();
-  console.log(data);
-  data.map(item => {
-    const content = item.content;
-    content.map(item => {
-      console.log(item)
-    })
-  })
+  const res = await fetch('http://localhost:3000/schedules');
+  const datas = await res.json();
+  
 }
 
-//fetctData();
+fetctData();
 
