5ba3567405c22e9043740fe72b81e3ff6589b4ff
moise <mose.ns@hotmail.com>
2021-02-28 17:02:16 +0100

    Fetching and displaying schedule tuesday

--- modules/displayMondaySchedule.js
+++ modules/displayMondaySchedule.js
@@ -1,6 +1,6 @@
 import { fetchMondayData } from '../src/dataFetching.js';
 
-const image_text = document.querySelector('.image-text');
+const shedule_monday = document.querySelector('#monday');
 
 
 const mondaySchedule = async () => {
@@ -9,16 +9,18 @@ const mondaySchedule = async () => {
     const newMonday = mondays.map(monday => {
       const { image, title, subtitle, hour, body } = monday;
       return `
-      <img src="${image}" alt="${title}" title="${title}">
-      <article class="schedule-texts">
-        <h3>${title}</h3>
-        <h4>${subtitle}</h4>
-        <h4>${hour}</h4>
-        <p>${body}</p>
-      </article>
+      <div class="image-text">
+        <img src="${image}" alt="${title}" title="${title}">
+        <article class="schedule-texts">
+          <h3>${title}</h3>
+          <h4>${subtitle}</h4>
+          <h4>${hour}</h4>
+          <p>${body}</p>
+        </article>
+      </div>
       `;
     }).join('');
-   image_text.innerHTML = newMonday;
+   shedule_monday.innerHTML = newMonday;
     
   }catch(error){
     console.log('Unable to fetch monday datas');
