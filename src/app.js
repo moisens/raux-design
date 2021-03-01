033671c3c06ccdab6a226c8870b6d325365a518f
moise <mose.ns@hotmail.com>
2021-03-01 15:34:22 +0100

    schedule js functionalities

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import Wednesday_schedule from '../modules/displayWednesdaySchedule.js';
 import thursdaySchedule from '../modules/displayThursdaySchedule.js';
 import fridaySchedule from '../modules/displayFridaySchedule.js';
 import nextMondaySchedule from '../modules/displayNextMondaySchedule.js'
+import showSchedule from '../modules/sheduleTab.js'
 
 
 const init = () => {
@@ -21,3 +22,11 @@ const init = () => {
 }
 
 window.addEventListener('load', init);
+
+
+showSchedule
+
+
+
+
+
