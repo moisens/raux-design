61ea70248a960d807362c2e6c5d8601395259d67
moise <mose.ns@hotmail.com>
2021-03-21 15:11:16 +0100

    schedule component PartI

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,6 +1,12 @@
+import './styles.scss'
+import nav from '../components/nav/nav.js';
+import scheduling from '../components/schedule/schedul.js'
+import footer from '../components/footer/footer.js'
+
 const Schedule = () => {
-  console.log('schedule Page');
+  nav();
+  scheduling();
+  footer();
 }
 Schedule();
-
 
