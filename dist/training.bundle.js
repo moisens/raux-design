5e0b24a002803ac58047ba79d1cfd74ca256884a
moise <mose.ns@hotmail.com>
2021-03-27 02:40:17 +0100

    styling cart component

--- dist/training.bundle.js
+++ dist/training.bundle.js
@@ -606,7 +606,7 @@ eval("module.exports = __webpack_require__.p + \"d19ece4428bdf8fb6922.svg\";\n\n
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("3dc61ab408a436217f77")
+/******/ 		__webpack_require__.h = () => ("b9bac099af2d98894a9a")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
